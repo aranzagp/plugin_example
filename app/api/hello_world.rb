fefc684bb05f20433a077c20c379af0cf94709ac
aranza <aranza@michelada.io>
2018-04-05 11:56:18 -0500

    remove module

--- app/api/hello_world.rb
+++ app/api/hello_world.rb
@@ -1,12 +1,10 @@
-module API
-  class HelloWorld < Grape::API
-    # Uncomment this to require authentication.
-    # before { authenticate! }
+class HelloWorld < Grape::API
+  # Uncomment this to require authentication.
+  # before { authenticate! }
 
-    desc 'Returns a hello world message.'
-    get '/hello_world' do
-      body 'Hello World!'
-      status 200
-    end
+  desc 'Returns a hello world message.'
+  get '/hello_world' do
+    body 'Hello World!'
+    status 200
   end
 end
